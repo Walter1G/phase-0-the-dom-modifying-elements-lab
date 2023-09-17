444af12032c79471a9e9b732b09610f479a234cd
Walter N <2760walter@gmail.com>
2023-09-17 11:05:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+//add new header
+
+const newHeader = document.createElement("h1");
+
+newHeader.id="victory";
+newHeader.textContent="Walter is the champion";
+document.body.append(newHeader);
